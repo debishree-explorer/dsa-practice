c66f26d5a3ade1de8ea450106f1bf420b460cbc9
debishree <debishree1833@gmail.com>
2026-04-06 22:42:28 +0530

    twoSum program using Array

---
+++ twoSum.java
@@ -0,0 +1,32 @@
+package DsaAll;
+
+public class twoSum {
+
+	    public static void findTwoSum(int[] arr, int target) {
+	        boolean found = false;
+
+	        for (int i = 0; i < arr.length; i++) {
+	            for (int j = i + 1; j < arr.length; j++) {
+
+	                if (arr[i] + arr[j] == target) {
+	                    System.out.println("Indices: " + i + ", " + j);
+	                    System.out.println("Values: " + arr[i] + ", " + arr[j]);
+	                    found = true;
+	                    break;
+	                }
+	            }
+	            if (found) break;
+	        }
+
+	        if (!found) {
+	            System.out.println("No solution found");
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        int[] arr = {2, 7, 11, 15};
+	        int target = 9;
+
+	        findTwoSum(arr, target);
+	    }
+}
