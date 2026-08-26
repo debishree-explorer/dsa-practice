6652b7eb165825fb2870e8d49dc82041bd0e2017
Debishree <debishree1833@gmail.com>
2026-08-26 22:29:50 +0530

    deleted files

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -1,27 +1,20 @@
-public class SecondLargest{
+package dsapractice.Arrays;
+
+public class secondLargest {
     public static void main(String[] args){
-        int a[] = {10,90,20,100,40,60};
-        int result = secondLargest(a);
-        System.out.println("the second largest is:"+result);
-    }
-    static int secondLargest(int a[]){
-        int lar , seclar;
-        if(a[0] > a[1]){
-            lar = a[0];
-            seclar = a[1];
-        }else {
-            lar = a[1];
-            seclar = a[0];
-        }
-        for(int i=2 ; i<a.length ; i++){
-            if(a[i] > lar){
-                seclar = lar;
-                lar = a[i];
-            }else if(a[i] > seclar && a[i]!= lar){
-                seclar = a[i];
+        int[] arr = {10,20,30,80,50};
+        int larg = Integer.MIN_VALUE;
+        int secLarg = Integer.MIN_VALUE;
+
+        for(int i=0 ; i<arr.length ; i++){
+            if(arr[i] > larg){
+                secLarg = larg;
+                larg = arr[i];
+            }
+            else if(arr[i] > secLarg && arr[i] != larg){
+                secLarg = arr[i];
             }
         }
-        return seclar;
+        System.out.println("second largest is:"+secLarg);
     }
-
 }
